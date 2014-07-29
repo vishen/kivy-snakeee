39885fe701cfef323aacd5fa8ccfb203538ba7ca
Jonathan Pentecost <pentecostjonathan@gmail.com>
2014-07-29 17:12:15 +0200

    Initial commit

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ from kivy.app import App
 from kivy.core.window import Window
 from kivy.clock import Clock
 from kivy.graphics import Color, Rectangle
+from kivy.properties import NumericProperty
+from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 
-SPEED = 0.3
+SPEED = 0.1
 
 class Snake(Widget):
 
@@ -20,18 +23,65 @@ class Snake(Widget):
 		"down": "up",
 	}
 
-	food_pos = None
-
 	direction = "down"
-	body = [[300, 300], [330, 300], [360, 300]]
+	body = []
 	body_size = 30.
+	food = None
+
+	points = NumericProperty(0)
+	score = None
 
 	def __init__(self, *args, **kwargs):
 		super(Snake, self).__init__(*args, **kwargs)
 
 		self._keyboard = Window.request_keyboard(self._keyboard_closed, self, "text") 
 		self._keyboard.bind(on_key_down=self._on_keyboard_down)
-		# self.update()
+
+		self._initialize_game()
+
+	def _initialize_game(self):
+		self.body = []
+		self.score = None
+		self.food = None
+		self.points = 0
+		self.direction = "down"
+
+		self.canvas.clear()
+		size = self.body_size
+		with self.canvas:
+			Color(0, 0, 1)
+			head = Rectangle(pos=[300, 300], size=(size, size))
+			self.body.append(head)
+
+
+			x, y = self.generate_food_position()
+
+			Color(0, 1, 0)
+			self.food = Rectangle(pos=[x, y], size=(size, size))
+
+		self.score = Label(font_size=70, text=str(self.points))
+		self.add_widget(self.score)
+
+
+	def generate_food_position(self):
+
+		size = self.body_size
+		window_size = Window.size
+
+		while True:
+			x = random.randint(size, window_size[0] - size)
+			y = random.randint(size, window_size[1] - size)
+
+			x -= (x % size)
+			y -= (y % size)
+
+			found = False
+			for b in self.body:
+				if b.pos == [x, y]:
+					found = True
+
+			if not found:
+				return x, y
 
 	def _keyboard_closed(self):
 		self._keyboard.unbind(on_key_down=self.on_key_down)
@@ -41,20 +91,25 @@ class Snake(Widget):
 		_direction = keycode[1]
 		if self.unusable_directions[_direction] != self.direction:
 			self.direction = _direction
-			# self.update()
 
-	def _update_body(self, front_pos):
+	def _update_body(self, front_pos, add_new=False):
 		next_pos = front_pos
 		for i in range(len(self.body)):
-			tmp = self.body[i]
-			self.body[i] = next_pos
+			tmp = self.body[i].pos
+			self.body[i].pos = next_pos
 			next_pos = tmp
+
+		if add_new:
+			with self.canvas:
+				Color(0, 0, 1)
+				bp = Rectangle(pos=next_pos, size=(self.body_size, self.body_size))
+				self.body.append(bp)
 
 	def update(self, dt):
 		
 		size = self.body_size
 
-		new_pos = list(self.body[0])
+		new_pos = list(self.body[0].pos)
 
 		if self.direction == "right":
 			new_pos[0] += size
@@ -65,28 +120,61 @@ class Snake(Widget):
 		elif self.direction == "down":
 			new_pos[1] -= size
 
-		self._update_body(new_pos)
+		game_over = False
+		window_size = Window.size
+		if new_pos[0] < 0 or new_pos[1] < 0:
+			game_over = True
 
-		self.canvas.clear()
-		with self.canvas:
-			# Draw food.
-			if not self.food_pos:
-				x = random.randint(1, self.size[1])
-				y = random.randint(1, self.size[0])
-				
-				self.food_pos = [x, y]
-			Color(0, 1, 0)
-			Rectangle(pos=self.food_pos, size=(size, size))
+		elif new_pos[0] + size > window_size[0] or\
+			  new_pos[1] + size > window_size[1]:
+			  game_over = True
 
-			# Draw body first.
-			Color(1, 0, 0)
-			if len(self.body) > 1:
-				for b in self.body[1:]:
-					Rectangle(pos=b, size=(size, size))
+		for b in self.body[1:]:
+			if list(b.pos) == new_pos:
+				game_over = True
 
-			# Draw head last
-			Color(0, 0, 1)
-			Rectangle(pos=self.body[0], size=(size, size))
+
+		if game_over:
+			Clock.unschedule(self.update)
+			self.canvas.clear()
+			l = Label(font_size=20, pos=(300, 300), 
+					text="Game Over! Score: %d points" % self.points)
+
+			def _play_again(*args, **kwargs):
+				self._initialize_game()
+				Clock.schedule_interval(self.update, SPEED)
+
+			b = Button(text="Play Again?")
+			b.on_release = _play_again
+			self.add_widget(l)
+			self.add_widget(b)
+
+			return
+
+		food_pos = self.food.pos
+		ate_food = False
+		if food_pos[0]  in (new_pos[0], new_pos[0] + size) and \
+			food_pos[1] in (new_pos[1], new_pos[1] + size):
+
+			self.points += 1
+			self.score.text = str(self.points)
+
+			x, y = self.generate_food_position()
+
+			self.food.pos = [x, y]
+
+			ate_food = True
+
+
+
+		self._update_body(new_pos, ate_food)
+
+			
+
+			
+
+
+		
 
 
 class GameApp(App):
